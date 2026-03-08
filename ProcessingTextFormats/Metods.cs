4a4676f82de529f9404d6aad04e34299c4a9c6e9
Sergey Lyadov <ly4dov.s@yandex.ru>
2026-03-08 22:20:49 +0300

    add methods Yaml and Csv convert, create test methods

--- ProcessingTextFormats/Metods.cs
+++ ProcessingTextFormats/Metods.cs
@@ -1,9 +1,13 @@
-﻿using Newtonsoft.Json;
+﻿using CsvHelper;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Xml;
+using System.Linq;
+using System.Text;
 using System.Xml.Serialization;
+using YamlDotNet.Serialization;
 
 
 interface FileAction<T>
@@ -39,10 +43,10 @@ public class XmlMethods<T> : FileAction<T>
 {
     public List<T> ReadFile(string fileName)
     {
-        XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+        XmlSerializer deserializer = new XmlSerializer(typeof(List<T>));
         using (StreamReader reader = new StreamReader(fileName))
         {
-            return (List<T>)serializer.Deserialize(reader);
+            return (List<T>)deserializer.Deserialize(reader);
         }
     }
 
@@ -57,6 +61,53 @@ public class XmlMethods<T> : FileAction<T>
     }
 }
 
+
+public class YamlMethods<T> : FileAction<T>
+{
+    public List<T> ReadFile(string fileName)
+    {
+        string dataStr = "";
+        var deserializer = new DeserializerBuilder().Build();
+        using (StreamReader reader = new StreamReader(fileName))
+        {
+            dataStr = reader.ReadToEnd();
+            return deserializer.Deserialize<List<T>>(dataStr);
+        }
+    }
+
+    public void WriteFile(List<T> data, string fileName)
+    {
+
+        var serializer = new SerializerBuilder().Build();
+        using (StreamWriter writer = new StreamWriter(fileName))
+        {
+            string dataStr = serializer.Serialize(data);
+            writer.Write(dataStr);
+        }
+    }
+}
+
+public class CsvMethods<T> : FileAction<T>
+{
+    public List<T> ReadFile(string fileName)
+    {
+        using (StreamReader reader = new StreamReader(fileName, Encoding.UTF8))
+        using (CsvReader csv = new CsvReader(reader, CultureInfo.CurrentCulture))
+        {
+            return csv.GetRecords<T>().ToList();
+        }
+    }
+
+    public void WriteFile(List<T> data, string fileName)
+    {
+        var serializer = new SerializerBuilder().Build();
+        using (StreamWriter writer = new StreamWriter(fileName,false, Encoding.UTF8))
+        using(CsvWriter csv = new CsvWriter(writer, CultureInfo.CurrentCulture))
+        {
+            csv.WriteRecords(data);
+        }
+    }
+}
 
 public class Metods
 {
--- Tests/UnitTest.cs
+++ Tests/UnitTest.cs
@@ -7,8 +7,10 @@ public class SimpleJsonTest
 {
     public class Person
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Email { get; set; }
     }
 
     [Test]
@@ -20,8 +22,8 @@ public class SimpleJsonTest
 
         var peopleToWrite = new List<Person>
             {
-                new Person { Name = "Иван", Age = 30 },
-                new Person { Name = "Мария", Age = 25 }
+                new Person { Id = 1, Name = "Иван", Age = 30, Email = "ivan@mail.com" },
+            new Person { Id = 2, Name = "Мария", Age = 25, Email = "maria@mail.com" }
             };
 
         //ЗАПИСЬ
@@ -51,8 +53,39 @@ public class SimpleJsonTest
 
         var peopleToWrite = new List<Person>
             {
-                new Person { Name = "Иван", Age = 30 },
-                new Person { Name = "Мария", Age = 25 }
+               new Person { Id = 1, Name = "Иван", Age = 30, Email = "ivan@mail.com" },
+                new Person { Id = 2, Name = "Мария", Age = 25, Email = "maria@mail.com" }
+        };
+
+        //ЗАПИСЬ
+        method.WriteFile(peopleToWrite, testFile);
+
+        //ЧТЕНИЕ
+        var peopleRead = method.ReadFile(testFile);
+
+        //ПРОВЕРКА
+        Assert.That(peopleRead.Count, Is.EqualTo(2));
+        Assert.That(peopleRead[0].Name, Is.EqualTo("Иван"));
+        Assert.That(peopleRead[0].Age, Is.EqualTo(30));
+        Assert.That(peopleRead[1].Name, Is.EqualTo("Мария"));
+        Assert.That(peopleRead[1].Age, Is.EqualTo(25));
+
+        //УДАЛЕНИЕ
+        //if (File.Exists(testFile))
+        //    File.Delete(testFile);
+    }
+
+    [Test]
+    public void CanWriteAndReadYamlFile()
+    {
+
+        YamlMethods<Person> method = new YamlMethods<Person>();
+        var testFile = "test.yaml";
+
+        var peopleToWrite = new List<Person>
+            {
+                new Person { Id = 1, Name = "Иван", Age = 30, Email = "ivan@mail.com" },
+            new Person { Id = 2, Name = "Мария", Age = 25, Email = "maria@mail.com" }
             };
 
         //ЗАПИСЬ
@@ -72,4 +105,36 @@ public class SimpleJsonTest
         //if (File.Exists(testFile))
         //    File.Delete(testFile);
     }
+
+    [Test]
+    public void CanWriteAndReadCsvFile()
+    {
+
+        CsvMethods<Person> method = new CsvMethods<Person>();
+        var testFile = "test.csv";
+
+        var peopleToWrite = new List<Person>
+            {
+                new Person { Id = 1, Name = "Иван", Age = 30, Email = "ivan@mail.com" },
+            new Person { Id = 2, Name = "Мария", Age = 25, Email = "maria@mail.com" }
+            };
+
+        //ЗАПИСЬ
+        method.WriteFile(peopleToWrite, testFile);
+
+        //ЧТЕНИЕ
+        var peopleRead = method.ReadFile(testFile);
+
+        //ПРОВЕРКА
+        Assert.That(peopleRead.Count, Is.EqualTo(2));
+        Assert.That(peopleRead[0].Name, Is.EqualTo("Иван"));
+        Assert.That(peopleRead[0].Age, Is.EqualTo(30));
+        Assert.That(peopleRead[1].Name, Is.EqualTo("Мария"));
+        Assert.That(peopleRead[1].Age, Is.EqualTo(25));
+
+        //УДАЛЕНИЕ
+        //if (File.Exists(testFile))
+        //    File.Delete(testFile);
+    }
+
 }
