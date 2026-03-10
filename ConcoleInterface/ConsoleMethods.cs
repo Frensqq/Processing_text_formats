5412ac1b254f92084832dd5afabc2fac111f9240
frens <liadovn@yandex.ru>
2026-03-10 09:26:06 +0300

    create input file name methods

--- ConcoleInterface/ConsoleMethods.cs
+++ ConcoleInterface/ConsoleMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,7 @@ namespace ConcoleInterface
         {
             Console.WriteLine("Список доступных действий:");
             Console.WriteLine("1) Запись данных в файл // 2) Импорт данных из файла // 3) Сортировка // 4) Поиск // 0) Выход");
+            Console.Write("Ввод:");
             int menu = Convert.ToInt16(Console.ReadLine());
 
             if (menu < 0 || menu > 4)
@@ -25,6 +27,68 @@ namespace ConcoleInterface
                 return menu;
             }
         }
+
+        public void StartMethods(int task)
+        {
+            InputsMetods inputsMetods = new InputsMetods();
+            switch (task)
+            {
+                case 1:
+                    string fileName = inputsMetods.addFileName();
+                    int codeType = TypeFileMenu();
+                    if (codeType != 1)
+                    {
+                        fileName = inputsMetods.addType(fileName, codeType);
+                    }
+
+                    break;
+                case 2:
+
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+
+                    break;
+                default:
+                    Console.WriteLine("Error add type file");
+                    break;
+
+            }
+        }
+
+        public int TypeFileMenu()
+        {
+            Console.WriteLine("Выберите расширение файла");
+            Console.WriteLine("1) Json // 2) XML // 3) CSV // 4) Yaml");
+            Console.Write("Ввод: ");
+            int type = Convert.ToInt16(Console.ReadLine());
+
+            if (type < 1 || type > 4)
+            {
+                Console.WriteLine($"Раширение под значением {type} несуществует!\nВведите число от 1 до 4!");
+                return -1;
+            }
+            else
+            {
+                return type;
+            }
+        }
+        
+    } 
+
+    public class InputsMetods
+    {
+        public string addFileName()
+        {
+            Console.WriteLine("Введите путь создания с именем файла на конце.");
+            Console.Write("Ввод:");
+            string fileName = Console.ReadLine();
+            return fileName;
+        }
+
 
         public string addType(string fileName, int TypeFile)
         {
@@ -51,7 +115,7 @@ namespace ConcoleInterface
                 default:
                     Console.WriteLine("Error add type file");
                     break;
-                   
+
             }
             return file;
         }
--- ConcoleInterface/Program.cs
+++ ConcoleInterface/Program.cs
@@ -1,4 +1,11 @@
-﻿
+﻿using ConcoleInterface;
 
+ConsoleMethods consoleMethods = new ConsoleMethods();
 
+while (true)
+{
+    int task = consoleMethods.Menu();
 
+    if (task == 0) break;
+    if (task == -1) continue;
+}
