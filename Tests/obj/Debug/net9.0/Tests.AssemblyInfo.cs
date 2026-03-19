91933755450a76cc59a2685a3a82e87ca8d1316e
Sergey Lyadov <ly4dov.s@yandex.ru>
2026-03-19 20:15:39 +0300

    current state

--- Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net9.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ce372be1f559dcf3d4124545690953fa6785ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+766c30b840d407ff7c42c65806a2d777b0b48849")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
