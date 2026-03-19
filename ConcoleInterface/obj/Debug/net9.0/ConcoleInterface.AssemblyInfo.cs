766c30b840d407ff7c42c65806a2d777b0b48849
Sergey Lyadov <ly4dov.s@yandex.ru>
2026-03-19 16:48:10 +0300

    refactoring code

--- ConcoleInterface/obj/Debug/net9.0/ConcoleInterface.AssemblyInfo.cs
+++ ConcoleInterface/obj/Debug/net9.0/ConcoleInterface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConcoleInterface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa5382251524bb898f892f2d97b93e111a5d385b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ce372be1f559dcf3d4124545690953fa6785ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConcoleInterface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConcoleInterface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
